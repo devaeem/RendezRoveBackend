c65b96e38f55bfbed220a1fb9295ac7058c4ed63
aeem <tattapan.wan@everapp.io>
2024-01-06 18:54:15 +0700

    fix sucess

--- dist/index.js
+++ dist/index.js
@@ -13,19 +13,4 @@ app.get('/', (req, res) => {
 app.get('/test', (req, res) => {
     res.json({ data: 'test' });
 });
-const swaggerJSDoc = require('swagger-jsdoc');
-const swaggerDefinition = {
-    openapi: '3.0.0',
-    info: {
-        title: 'rendezrove API ',
-        version: '1.0.0',
-    },
-};
-const options = {
-    swaggerDefinition,
-    // Paths to files containing OpenAPI definitions
-    apis: ['./routes/*.ts'],
-};
-const swaggerSpec = swaggerJSDoc(options);
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.listen(port, () => console.log(`Application is running on port ${port}`));
--- src/index.ts
+++ src/index.ts
@@ -18,25 +18,7 @@ app.get('/test', (req: Request, res: Response) => {
     {data:'test'}
   )
 })
-const swaggerJSDoc = require('swagger-jsdoc');
-const swaggerDefinition = {
-    openapi: '3.0.0',
-    info: {
-      title: 'rendezrove API ',
-      version: '1.0.0',
-    },
-  };
-  
-  const options = {
-    swaggerDefinition,
-    // Paths to files containing OpenAPI definitions
-    apis: ['./routes/*.ts'],
-  };
-  
-  const swaggerSpec = swaggerJSDoc(options);
 
-
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
 app.listen(port, () => console.log(`Application is running on port ${port}`))
