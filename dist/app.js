fae5721111016c61023aba160ae53e4d4900318d
aeem <tattapan.wan@everapp.io>
2024-01-06 17:09:20 +0700

    fix deploy

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,12 @@
 "use strict";
-console.log('test');
-let first = 'dd';
-console.log('first', first);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 5001;
+app.get('/', (req, res) => {
+    res.send('Hello rendezrovebackendservice + TypeScirpt!!');
+});
+app.listen(port, () => console.log(`Application is running on port ${port}`));
