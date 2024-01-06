d367c877f3b6a382c16863c25d35cbf4f52d1c85
aeem <tattapan.wan@everapp.io>
2024-01-06 18:28:28 +0700

    add docs api ui

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ const app: Express = express()
 
 const port: number = 5001
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./docs/docs');
+
 app.get('/', (req: Request, res: Response) => {
   res.send(
     'Hello rendezrovebackendservice + TypeScirpt!!',
@@ -16,5 +19,8 @@ app.get('/test', (req: Request, res: Response) => {
     {data:'test'}
   )
 })
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 
 app.listen(port, () => console.log(`Application is running on port ${port}`))
